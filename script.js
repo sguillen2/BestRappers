9ac34562d4dd17fa9cbad28da2cedac7967efca8
sguillen2 <45013630+sguillen2@users.noreply.github.com>
2019-11-08 15:29:12 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// call this function when "orange-kid" is clicked!
+function moveGif(elem){
+	elem.classList.add("fly-forward");
+}
+
+// call this function when "blue-man" is clicked!
+function changePicture(elem){
+	elem.src = "images/vegeta1.png";
+}
